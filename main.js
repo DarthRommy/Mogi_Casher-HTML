6540748da4754a7451db72398041390a8af3435d
DarthRommy <88261399+DarthRommy@users.noreply.github.com>
2021-09-14 00:31:07 +0900

    v0.2

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 /** ----D&D---- */
+function dropFile() {
+    $(".dropFile").modal();
+};
+
 const dropArea = document.getElementById("drop");
 
 dropArea.addEventListener("dragover", function(e){
     e.preventDefault();
 });
-
+dropArea.addEventListener("dragleave", function(e){});
 dropArea.addEventListener("drop", function(e){
     e.preventDefault();
 
@@ -42,42 +46,88 @@ function getFiles(files) {
         // テキストの読み込みが完了した際のイベントを登録
         reader.onload = (event) => {
             const text = event.target.result;
-    
-            const csv = text.split("\r\n").map((row) => row.split(","));
             
-            makeTable(csv, "table")
+            const csv = text.split("\r\n").map((row) => row.split(","));
+
+            makeTable(csv, "table", "preview-csv")
         };
     }
     
 };
 
-function makeTable(data, divname){
+
+function makeTable(data, target, tableId){
     /** ----配列⇒Table */    
     var tableText=[];
+    console.log(data)
 
     for(i=0;i<data.length;i++) {
         tableText.push(data[i]);
+
+        const onerow = data[i]
+        key = "row"+i
+        sessionStorage.setItem(key, onerow.join())
     };
 
+    sessionStorage.setItem("length", String(data.length))
+
     var rows=[];
-    var table = document.createElement(divname);
+    var table = document.createElement(target);
 
     for(i=0; i<tableText.length; i++) {
         // 行の追加
         rows.push(table.insertRow(-1));
+        
         for(j=0; j<4; j++) {
             // 追加した行にセルを追加してテキストを書き込む
             cell=rows[i].insertCell(-1);
             cell.appendChild(document.createTextNode(tableText[i][j]));
         }
-    }
-    table.id = "preview-csv";
+    };
+
+    table.id = tableId;
     table.border = 1;
-    document.getElementById(divname).appendChild(table);
+    document.getElementById(target).appendChild(table);
 };
 
-function dropFile() {
-    $(".dropFile").modal();
+/** ----時計---- */
+function twoDigit(num) {
+    let ret;
+    if( num < 10 ) 
+      ret = "0" + num; 
+    else 
+      ret = num; 
+    return ret;
+}
+function showClock() {
+    let nowTime = new Date();
+    let nowHour = twoDigit( nowTime.getHours() );
+    let nowMin  = twoDigit( nowTime.getMinutes() );
+    let nowSec  = twoDigit( nowTime.getSeconds() );
+    let msg = nowHour + ":" + nowMin + ":" + nowSec;
+    document.getElementById("clock").innerHTML = msg;
+}
+setInterval('showClock()',1000);
+
+/** ----databaseテーブルの表示切替---- */
+function showBool() {
+    var boole = document.getElementById("chk").checked;
+    var showTable = document.getElementById("database");
+
+    if (boole == true){
+        showTable.style.visibility = "visible"
+    }else{
+        showTable.style.visibility = "hidden"
+    }
+
+    console.log(boole)
+}
+setInterval("showBool()", 500)
+
+/** ----ロード時アニメーション---- */
+window.onload = function() {
+    const spinner = document.getElementById('loading');
+    spinner.classList.add('loaded');
 };
 
 /**---- 入力欄周り ----*/
@@ -112,33 +162,14 @@ function clearStoreName() {
     document.getElementById("inputStoreName").value = "";
 };
 
-/** ----ロード時アニメーション---- */
-window.onload = function() {
-    const spinner = document.getElementById('loading');
-    spinner.classList.add('loaded');
-};
+function submitStoreName(){
+    var storeName = document.getElementById("inputStoreName").value;
 
-/** ----時計---- */
-function twoDigit(num) {
-    let ret;
-    if( num < 10 ) 
-      ret = "0" + num; 
-    else 
-      ret = num; 
-    return ret;
+    var dispName = document.getElementById("name");
+    dispName.innerHTML = storeName
 }
-function showClock() {
-    let nowTime = new Date();
-    let nowHour = twoDigit( nowTime.getHours() );
-    let nowMin  = twoDigit( nowTime.getMinutes() );
-    let nowSec  = twoDigit( nowTime.getSeconds() );
-    let msg = nowHour + ":" + nowMin + ":" + nowSec;
-    document.getElementById("clock").innerHTML = msg;
-}
-setInterval('showClock()',1000);
 
-/** ----リロード禁止!---- 
-$(window).on('beforeunload', function(e) {
-    return 'Save Before Close';
-});
-*/
+
+function aaa(){
+    console.log("hello")
+}
